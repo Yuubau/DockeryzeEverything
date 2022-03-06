fd706d8882c322b05da6dddc011b4378189f9237
oka.jeanjaures@gmail.com <oka.jeanjaures@gmail.com>
2022-03-06 17:42:40 +0100

    postRequest

--- src/src/controller/books_controller.php
+++ src/src/controller/books_controller.php
@@ -16,3 +16,61 @@ function parse_get_request($uri) {
         return $res;
     }
 }
+
+
+function parse_post_request($uri) {
+    $isbn;
+    $title;
+    $author;
+    $overview;
+    $read_count;
+
+    $error = "";
+
+    if(isset($_POST["isbn"]) && strlen($_POST["isbn"]) <= 13){
+        $isbn = $_POST["isbn"];
+    } else {
+        $error .= "Invalid ISBN<BR>";
+    }
+    if(count(findBookByIsbn($isbn)) > 0){
+        $error .= "ISBN already exists<BR>";
+    }
+    if(isset($_POST["title"]) && strlen($_POST["title"]) <= 200){
+        $title = $_POST["title"];
+    } else {
+        $error .= "Invalid title<BR>";
+    }
+    if(isset($_POST["author"]) && strlen($_POST["author"]) <= 150){
+        $author = $_POST["author"];
+    } else {
+        $error .= "Invalid author<BR>";
+    }
+    if(isset($_POST["overview"])){
+        if(strlen($_POST["overview"]) <= 1500){
+            $overview = $_POST["overview"];
+        } else {
+            $error .= "Invalid overview<BR>";
+        }
+    } else {
+        $overview = "";
+    }
+    if(isset($_POST["read_count"])){
+        $read_count = $_POST["read_count"];
+    } else {
+        $read_count = 1;
+    }
+
+    if($error === ""){
+        $res = insertBook($isbn, $title, $author, $overview, $read_count);
+        if($res){
+            http_response_code(201);
+            return findBookByIsbn($isbn);
+        } else {
+            http_response_code(422);
+            return "An error has occured";
+        }
+    } else {
+        http_response_code(422);
+        return $error;
+    }
+}
