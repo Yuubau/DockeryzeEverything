5c363bbacabf8b6798fe6c1f5c6b1131391240ed
oka.jeanjaures@gmail.com <oka.jeanjaures@gmail.com>
2022-03-06 17:45:24 +0100

    patchRequest

--- src/src/controller/books_controller.php
+++ src/src/controller/books_controller.php
@@ -93,3 +93,73 @@ function parse_delete_request($uri) {
         return "An error has occured";
     }
 }
+
+function parse_patch_request($uri) {
+    $isbn;
+    $title;
+    $author;
+    $overview;
+    $read_count;
+
+    $error = "";
+
+    $book = findBookByIsbn($uri[1]);
+    if(count($book) < 1){
+        $error .= "ISBN does not exists<BR>";
+    } else {
+        $book = $book[0];
+    }
+
+    if(isset($_PATCH["isbn"])){
+        $isbn = $_PATCH["isbn"];
+        if(strlen($_PATCH["isbn"]) <= 13){
+            $error .= "Invalid ISBN<BR>";
+        }
+    } else {
+        $isbn = $book["isbn"];
+    }
+    if(isset($_PATCH["title"])){
+        $title = $_PATCH["title"];
+        if(strlen($_PATCH["title"]) <= 200){
+            $error .= "Invalid title<BR>";
+        }
+    } else {
+        $title = $book["title"];
+    }
+    if(isset($_PATCH["author"])){
+        $author = $_PATCH["author"];
+        if(strlen($_PATCH["author"]) <= 150){
+            $error .= "Invalid author<BR>";
+        }
+    } else {
+        $author = $book["author"];
+    }
+    if(isset($_PATCH["overview"])){
+        if(strlen($_PATCH["overview"]) <= 1500){
+            $overview = $_PATCH["overview"];
+        } else {
+            $error .= "Invalid overview<BR>";
+        }
+    } else {
+        $overview = $book["overview"];
+    }
+    if(isset($_PATCH["read_count"])){
+        $read_count = $_PATCH["read_count"];
+    } else {
+        $read_count = $book["read_count"];
+    }
+
+    if($error === ""){
+        $res = patchBook($uri[1],$isbn, $title, $author, $overview, $read_count);
+        if($res){
+            http_response_code(200);
+            return findBookByIsbn($isbn);
+        } else {
+            http_response_code(422);
+            return "An error has occured";
+        }
+    } else {
+        http_response_code(422);
+        return $error;
+    }
+}
